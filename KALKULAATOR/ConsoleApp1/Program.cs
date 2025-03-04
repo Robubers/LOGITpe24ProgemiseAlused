89a6f51f9bf4a46dcca1834c1b60d97aab26ccaf
Robubers <rp5176838@gmail.com>
2025-03-04 09:26:51 +0200

    kalkulaator

---
+++ KALKULAATOR/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Sisesta esimene arv:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Sisesta tehe (+, -, *, /):");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Sisesta teine arv:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            if (operation == '+')
+            {
+                result = num1 + num2;
+            }
+            else if (operation == '-')
+            {
+                result = num1 - num2;
+            }
+            else if (operation == '*')
+            {
+                result = num1 * num2;
+            }
+            else if (operation == '/')
+            {
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Tegemist on jagamisega nulliga, mis pole lubatud.");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Vigane tehe, palun kasuta +, -, * või /.");
+                return;
+            }
+
+            Console.WriteLine("Tulemus: " + result);
+        }
+    }
+
+
+}
+
